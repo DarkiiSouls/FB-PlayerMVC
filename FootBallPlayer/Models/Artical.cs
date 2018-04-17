933bb081775ef9c3d39fb13fe282d6bdb8b555c5
DarkIISouls <DarkIISouls@outlook.com>
2018-04-17 19:19:03 +0300

    articals

--- FootBallPlayer/Models/Artical.cs
+++ FootBallPlayer/Models/Artical.cs
@@ -1,13 +1,22 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace FootBallPlayer.Models
 {
     public class Artical
     {
+        [Key]
         public int ArticalId { get; set; }
         public string Text { get; set; }
+        public DateTime PublishDate { get; set; }
+
         public int PlayerId { get; set; }
         public int VisterId { get; set; }
+        public int ImagId { get; set; }
 
+        public virtual ApplicationUser Visiter { get; set; }
+        public virtual ApplicationUser Player { get; set; }
 
+        public virtual Imag imag { get; set; }
     }
 }
